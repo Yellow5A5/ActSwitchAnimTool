cabfbf1d9092f3bf9c133451c90d2709381540e0
Yellow5A5 <scnu.hww@foxmail.com>
2016-09-16 11:49:03 +0800

    project init ~

---
+++ app/src/androidTest/java/yellow5a5/actswitchanimtool/ApplicationTest.java
@@ -0,0 +1,13 @@
+package yellow5a5.actswitchanimtool;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

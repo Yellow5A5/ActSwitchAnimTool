c23db1ac015ceed9891992a90dbf0ce35c413119
Yellow5A5 <scnu.hww@foxmail.com>
2016-09-17 13:06:58 +0800

    add: the share demo of Anim.🙉

---
+++ sample/src/main/java/yellow5a5/sample/ShareDemo/ShareContainer.java
@@ -0,0 +1,25 @@
+package yellow5a5.sample.ShareDemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Yellow5A5 on 16/9/17.
+ */
+public class ShareContainer extends LinearLayout{
+    public ShareContainer(Context context) {
+        this(context, null);
+    }
+
+    public ShareContainer(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ShareContainer(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        LayoutInflater.from(context).inflate(R.layout.)
+        init();
+    }
+}
